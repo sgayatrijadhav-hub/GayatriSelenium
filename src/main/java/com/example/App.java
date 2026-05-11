b4462c37a3aca0c6dce15588a1a67ef2472a60b2
Gayatri Jadhav <sgayatrijadhav@gmail.com>
2026-05-11 07:56:28 +0530

    Fixed Jenkins Selenium

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,31 +1,10 @@
 package com.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class App {
 
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+        System.out.println("Selenium Maven Project Executed Successfully!");
 
-        driver.get("https://www.google.com");
-
-        System.out.println("Title: " + driver.getTitle());
-
-        WebElement searchBox =
-                driver.findElement(By.name("q"));
-
-        searchBox.sendKeys("Selenium Maven Project");
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.quit();
     }
 }
